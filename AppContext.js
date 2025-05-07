175c143eb18869e16ee8054d4b95a11c7be9656b
AdityaParuleker <91523894+AdityaParuleker@users.noreply.github.com>
2025-05-07 14:08:26 +0530

    Add files via upload

---
+++ AppContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from "react";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [theme, setTheme] = useState(true);
+  return (
+    <AppContext.Provider value={{ user, setUser, theme, setTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
